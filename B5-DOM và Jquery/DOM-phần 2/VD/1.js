432d9cf09228f275715b5c8859dbfcadc3578bda
Hieu Nguyen <huuhieu0962331881@gmail.com>
2020-05-19 22:42:05 +0700

    khoa javascript

---
+++ B5-DOM và Jquery/DOM-phần 2/VD/1.js
@@ -0,0 +1,16 @@
+﻿document.body.onload = addElement;
+
+function addElement () { 
+    // create a new div element 
+    var newDiv = document.createElement("h2"); 
+    // and give it some content 
+    var newContent = document.createTextNode("Hi there and greetings!"); 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);  
+
+    // add the newly created element and its content into the DOM 
+    var currentDiv = document.getElementById("div1"); 
+    document.body.insertBefore(newDiv, currentDiv ); 
+}
+
+// Phương thức createElement() 
